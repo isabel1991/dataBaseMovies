bd0db2e799a541858ceec2e00b02910ea450f77f
isabel1991 <63849789+isabel1991@users.noreply.github.com>
2020-10-28 10:16:17 +0100

    dia1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  movies: any;
+
+  constructor(private httpClient: HttpClient) {
+    this.movies= this.httpClient.get('https://raw.githubusercontent.com/isabel1991/dataBaseMovies/main/moviesJson.json');
+  }
 
 }
